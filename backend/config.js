967fa4bbf8bc78c0c57345bbc814f9ab3165a07c
Malki2001 <malkiyasodhara18@gmail.com>
2024-10-15 22:11:37 +0530

    create the book model and connect mongoDb

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL= 'mongodb+srv://priyantha1206:tiqPu6Gx11jgCFHQ@bookstore-mern.geo2u.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore-MERN';
