353c1daf6ba3268ca75c22f8fabed0f4cec40217
Malki2001 <malkiyasodhara18@gmail.com>
2024-10-16 00:37:18 +0530

    create pages and route them

--- backend/index.js
+++ backend/index.js
@@ -12,16 +12,16 @@ app.use(express.json());
 
 //middleware for handle CORS policy
 // 01. Allow all origins with default of cors(*)
-// app.use(cors());
+app.use(cors());
 
 //02.Allow custome origin
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods : [ 'GET','POST','PUT','DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods : [ 'GET','POST','PUT','DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 app.get('/',(request , response)=>{
     console.log(request);
